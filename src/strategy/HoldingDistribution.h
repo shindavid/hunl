54ed08006de745bc7d9e3440a39058064c3bf8d1
David Shin <shin.david@gmail.com>
2015-06-08 00:05:29 -0500

    add TODO

--- src/strategy/HoldingDistribution.h
+++ src/strategy/HoldingDistribution.h
@@ -12,8 +12,19 @@ namespace ps = pokerstove;
  */
 class HoldingDistribution {
 private:
-  std::unordered_map<HoldingSet, double, HoldingSetHashFunction> _weights;
-  double _total_weight;
+  /*
+   * TODO: Create a specialized hash map that is initialized with 91 hash buckets, one for each
+   * possible pair of (ps::Rank high, ps::Rank low). There are 91 such pairs because there are 13
+   * pocket pairs and (13 choose 2) = 78 possible non-pocket-pair rank-pairs. 
+   *
+   * Then, for each bucket, use a size-n array of (suit_mask, float) pairs. I'm pretty sure the max
+   * value of n that will be needed for our strategy will be something like n=4.
+   *
+   * With this specialization, we'll never have map resizings.
+   */
+  typedef std::unordered_map<HoldingSet, float, HoldingSetHashFunction> holding_map_t;
+  holding_map_t _weights;
+  float _total_weight;
 
 public:
   /*
