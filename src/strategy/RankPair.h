0475d580c86a75d1addb6aae5d9af98a10207169
David Shin <shin.david@gmail.com>
2015-06-10 19:41:54 -0500

    rank pair class

---
+++ src/strategy/RankPair.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "pokerstove/peval/Rank.h"
+
+#include <exception>
+
+namespace ps = pokerstove;
+
+/*
+ * A RankPair is an ordered pair of (ps::Rank, ps::Rank).
+ *
+ * There are 91 possible RankPair's: 13 pocket pairs and 13-choose-2 non-pocket-pairs.
+ */ 
+class __attribute__ ((__packed__)) RankPair {
+private:
+  /*
+   * _high >= _low
+   */
+  const ps::Rank _high : 4;
+  const ps::Rank _low : 4;
+  
+public:
+  RankPair(ps::Rank high, ps::Rank low) :
+    _high(high), _low(low) {}
+
+  uint8_t code() const {
+    return (_high.code() * (_high.code()+1)) / 2 + _low.code();
+  }
+
+  ps::Rank getHigh() const { return _high; }
+  ps::Rank getLow() const { return _low; }
+  bool isPocketPair() const { return _high==_low; }
+
+  static RankPair decode(uint8_t code) {
+    // TODO: best approach here is look-up table in 91-element array?
+    throw std::exception("implement me");
+  }
+  static const int NUM_RANK_PAIRS = 91;
+};
+static_assert(sizeof(RankPair)==1);
+
