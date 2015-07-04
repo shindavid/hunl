4a03be00f4402e91533b5361f11afbf978805634
David Shin <shin.david@gmail.com>
2015-07-04 12:29:11 -0500

    remove std::vector

--- src/engine/Board.h
+++ src/engine/Board.h
@@ -3,21 +3,27 @@
 #include "pokerstove/peval/Card.h"
 #include "pokerstove/peval/CardSet.h"
 
+#include <cstring>
+
 namespace ps = pokerstove;
 
 class Board {
 private:
   ps::CardSet _card_set;
-  std::vector<ps::Card> _cards;
+  ps::Card _cards[5];
 
 public:
   void add(ps::Card card) {
+    _cards[getSize()] = card;
     _card_set.insert(card);
-    _cards.push_back(card);
   }
 
   ps::CardSet getCards() const { return _card_set; }
   ps::Card getCard(int i) const { return _cards[i]; }
-  unsigned getSize() const { return _cards.size(); }
+  unsigned getSize() const { return _cards_set.size(); }
+
+  bool operator==(const Board& board) const {
+    return !memcmp(this->_cards, board._cards, sizeof(_cards));
+  }
 };
 
