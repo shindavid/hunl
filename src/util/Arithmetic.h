d7acffdc92c08f319cfeba2c1362a719ffcd0e7b
David Shin <shin.david@gmail.com>
2015-07-25 18:33:49 -0500

    arithmetic functions

--- src/util/Arithmetic.h
+++ src/util/Arithmetic.h
@@ -1,7 +1,68 @@
+#pragma once
+
 #include <cmath>
+#include <assert.h>
 
 float EPSILON = 0.000001;
 
 bool approximately_equal(float x, float y) {
   return fabs(x-y) < EPSILON;
 }
+
+/*
+ * http://forums.overclockers.co.uk/showthread.php?p=8773984
+ */
+float fast_inv_sqrt(float x) {
+  float xhalf = 0.5f*x;
+  int i = *(int*)&x; // get bits for floating value
+  i = 0x5f375a86- (i>>1); // gives initial guess y0
+  x = *(float*)&i; // convert bits back to float
+  x = x*(1.5f-xhalf*x*x); // Newton step, repeating increases accuracy
+  return x;
+}
+
+float fast_sqrt(float x) {
+  return 1.0 / fast_inv_sqrt(x);
+}
+
+/*
+ * Functions to encode/decode an ordered pair of ints into a single int.
+ *
+ * 1 0 -> 0
+ * 2 0 -> 1
+ * 2 1 -> 2
+ * 3 0 -> 3
+ * 3 1 -> 4
+ * 3 2 -> 5
+ * 4 0 -> 6
+ * ...
+ *
+ * In general, <k, 0> maps to a triangular number.
+ */
+namespace ordered_ints {
+  /*
+   * (x,y)->n, x>y>=0
+   */
+  int encode(int x, int y) {
+    assert(x>y);
+
+    return x*(x-1)/2 + y;
+  }
+
+  /*
+   * n->(x,y), x>y
+   *
+   * WARNING(dshin): Could break for large values of n due to approximate nature of fast_sqrt()
+   * function. 
+   */
+  std::pair<int,int> decode(int n) {
+    int floor_sqrt = (unsigned) floor(fast_sqrt(8*n+1.5));
+    int x = (floor_sqrt+1)/2;
+    int y = n - x*(x-1)/2;
+
+    assert(encode(x,y) == n);
+
+    return std::make_pair(x,y);
+  }
+}
+
