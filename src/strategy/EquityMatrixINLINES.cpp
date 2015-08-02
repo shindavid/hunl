acd58af5ec7a5e467bd7e234ca894ff08abf7392
David Shin <shin.david@gmail.com>
2015-08-02 18:04:05 -0500

    change EquityMatrix to use dynamic-sized matrices

--- src/strategy/EquityMatrixINLINES.cpp
+++ src/strategy/EquityMatrixINLINES.cpp
@@ -1,23 +1,24 @@
 
+template<int N> EquityMatrix<N>::EquityMatrix() : _M(N, N), _W(N, 2), _P(N, 2) {}
+
 template<int N> template<typename T>
 void EquityMatrix<N>::compute(HoldingMap<N,T>& map) {
-  Matrix<float, N, 2, ColMajor> W;
   /*
    * TODO(dshin) The order of for-loops here is optimized for cache-write access, but not for
    * cache-read access. There might be a better way to do this if we dig into the Eigen API.
    */
   for (int p=0; p<2; ++p) {
     for (int i=0; i<N; ++i) {
-      W(i,p) = map.getValue(i).weight[p];
+      _W(i,p) = map.getValue(i).weight[p];
     }
   }
 
-  Matrix<float, N, 2, ColMajor> P = _M * W;
+  _P = _M * W;
   
   // See above TODO comment.
   for (int p=0; p<2; ++p) {
     for (int i=0; i<N; ++i) {
-      map.getValue(i).equity[p] = P(i,p);
+      map.getValue(i).equity[p] = _P(i,p);
     }
   }
 }
